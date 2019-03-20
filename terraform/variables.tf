1b872ac5bc7b559459b757c7683a3ab2c9b78278
Jimmy Mesta <jimmy@mestamachine.io>
2019-03-20 12:14:33 -0700

    add vm tf file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,13 @@ variable "resource_group_name" {
   description = "The name of the resource group in which the resources are created"
   default     = ""
 }
+
+variable "application_port" {
+    description = "The port that you want to expose to the external load balancer"
+    default     = 80
+}
+
+variable "admin_password" {
+    description = "Default password for admin"
+    default = "Passwwoord11223344"
+}
---
+++ terraform/vm.tf
@@ -0,0 +1,109 @@
+resource "azurerm_lb" "vmss" {
+  name                = "vmss-lb"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.vmss.name}"
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = "${azurerm_public_ip.vmss.id}"
+  }
+
+  tags {
+    environment = "manicode"
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "bpepool" {
+  resource_group_name = "${azurerm_resource_group.vmss.name}"
+  loadbalancer_id     = "${azurerm_lb.vmss.id}"
+  name                = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_probe" "vmss" {
+  resource_group_name = "${azurerm_resource_group.vmss.name}"
+  loadbalancer_id     = "${azurerm_lb.vmss.id}"
+  name                = "ssh-running-probe"
+  port                = "${var.application_port}"
+}
+
+resource "azurerm_lb_rule" "lbnatrule" {
+  resource_group_name            = "${azurerm_resource_group.vmss.name}"
+  loadbalancer_id                = "${azurerm_lb.vmss.id}"
+  name                           = "http"
+  protocol                       = "Tcp"
+  frontend_port                  = "${var.application_port}"
+  backend_port                   = "${var.application_port}"
+  backend_address_pool_id        = "${azurerm_lb_backend_address_pool.bpepool.id}"
+  frontend_ip_configuration_name = "PublicIPAddress"
+  probe_id                       = "${azurerm_lb_probe.vmss.id}"
+}
+
+data "azurerm_resource_group" "image" {
+  name = "myResourceGroup"
+}
+
+data "azurerm_image" "image" {
+  name                = "myPackerImage"
+  resource_group_name = "${data.azurerm_resource_group.image.name}"
+}
+
+resource "azurerm_virtual_machine_scale_set" "vmss" {
+  name                = "vmscaleset"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.vmss.name}"
+  upgrade_policy_mode = "Manual"
+
+  sku {
+    name     = "Standard_DS1_v2"
+    tier     = "Standard"
+    capacity = 2
+  }
+
+  storage_profile_image_reference {
+    id="${data.azurerm_image.image.id}"
+  }
+
+  storage_profile_os_disk {
+    name              = ""
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_profile_data_disk {
+    lun          = 0
+    caching        = "ReadWrite"
+    create_option  = "Empty"
+    disk_size_gb   = 10
+  }
+
+  os_profile {
+    computer_name_prefix = "vmlab"
+    admin_username       = "azureuser"
+    admin_password       = "Passwword1234"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+    ssh_keys {
+      path     = "/home/azureuser/.ssh/authorized_keys"
+      key_data = "${file("~/.ssh/id_rsa.pub")}"
+    }
+  }
+
+  network_profile {
+    name    = "terraformnetworkprofile"
+    primary = true
+
+    ip_configuration {
+      name                                   = "IPConfiguration"
+      subnet_id                              = "${azurerm_subnet.vmss.id}"
+      load_balancer_backend_address_pool_ids = ["${azurerm_lb_backend_address_pool.bpepool.id}"]
+    }
+  }
+  
+  tags {
+    environment = "manicode"
+  }
+}
