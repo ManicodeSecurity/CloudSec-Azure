972e3998b1f61dd8a24ae12784d65d69df8b7e0d
Jimmy Mesta <jimmy@mestamachine.io>
2019-04-02 18:09:16 -0500

    terraform ip

--- 004-Terraform/terraform/vm.tf
+++ 004-Terraform/terraform/vm.tf
@@ -52,7 +52,6 @@ resource "azurerm_virtual_machine_scale_set" "vmss" {
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.vmss.name}"
   upgrade_policy_mode = "Manual"
-  primary             = true
 
   sku {
     name     = "Standard_DS1_v2"
@@ -101,6 +100,7 @@ resource "azurerm_virtual_machine_scale_set" "vmss" {
       name                                   = "IPConfiguration"
       subnet_id                              = "${azurerm_subnet.vmss.id}"
       load_balancer_backend_address_pool_ids = ["${azurerm_lb_backend_address_pool.bpepool.id}"]
+      primary                                = true
     }
   }
   
